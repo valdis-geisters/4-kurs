b10f5192d0798e91f1fce88bf994e63c42d01f97
Viktor V <latvit@gmail.com>
2014-12-05 23:03:40 +0200

    добавил отчет к 6 лабе по .net

--- Razrabotka v srede .NET/lab6/FinalizeDisposibleClass/FinalizeDisposibleClass/Program.cs
+++ Razrabotka v srede .NET/lab6/FinalizeDisposibleClass/FinalizeDisposibleClass/Program.cs
@@ -16,11 +16,15 @@ namespace FinalizeDisposibleClass
       var rw = new ResourceWrapper();
       Console.WriteLine(rw);
       Console.WriteLine(new string('-', 20));
+      
       rw.Dispose();
       rw.Dispose();
       rw.Dispose();
       rw.Dispose();
       ResourceWrapper rw2 = new ResourceWrapper();
+
+      GC.Collect();
+      //GC.SuppressFinalize(rw2);
 
       Console.ReadLine();
     }
